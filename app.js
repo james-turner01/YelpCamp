e7e2fdfc97073c089c1e7e3157573c00a944bd2d
James Turner <james.c.turner01@gmail.com>
2023-09-28 12:53:41 +0100

    Changing the port number used to be environment variable

--- app.js
+++ app.js
@@ -246,6 +246,11 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('errors', { err })
 })
 
-app.listen(3000, () => {
-    console.log("Serving On Port 3000")
+// setting our port number to be whatever Render sets process.env.PORT to
+// or if in dvelopment mode will use port 3000
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+    console.log(`Serving On Port ${port}`)
 })
